a2644b1ca8088d47ca472c868d7fcecd0e1bdc6e
tsh <dr.tallin@gmail.com>
2016-01-27 23:31:09 +0200

    table

--- scripts.js
+++ scripts.js
@@ -17,5 +17,19 @@ $( document ).ready(function() {
             result2 += Zoi + Cm + Me;
         }
         $("#calcResult").text(result1*result2);
+    });
+
+    $("#table-calc").click(function(e){
+        var rv1_1 = parseFloat($("#t1_1").val()),
+            rv1_2 = parseFloat($("#t1_2").val()),
+            rv2_1 = parseFloat($("#t2_1").val()),
+            rv2_2 = parseFloat($("#t2_2").val()),
+            resultRow1 = rv1_1*rv1_2,
+            resultRow2 = rv2_1*rv2_2,
+            resultSum = resultRow1 + resultRow2;
+
+        $("#table-result1").text(resultRow1);
+        $("#table-result2").text(resultRow2);
+        $("#table-result").text(resultRow1+resultRow2);
     })
 });
